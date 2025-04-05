5ecb17f49f3ba32f402d4414e69676ca73810a05
hmasdev <73353463+hmasdev@users.noreply.github.com>
2025-04-05 22:35:41 +0900

    Enhance holiday utilities with detailed docstrings for better clarity and usage guidance

--- src/pybizday_utils/month.py
+++ src/pybizday_utils/month.py
@@ -14,6 +14,16 @@ def is_biz_end_of_month(
     date: datetime.date,
     is_holiday: IsHolidayFuncType = global_default_holiday_discriminator,
 ) -> bool:
+    """Check if the given date is the last business day of the month.
+
+    Args:
+        date (datetime.date): Date to check.
+        is_holiday (IsHolidayFuncType, optional): Function to check if a date is a holiday.
+            Defaults to global_default_holiday_discriminator.
+
+    Returns:
+        bool: True if the date is the last business day of the month, False otherwise.
+    """  # noqa: E501
     if is_holiday(date):
         return False
     return date.month != (get_next_bizday(date, is_holiday)).month
@@ -23,6 +33,16 @@ def is_biz_start_of_month(
     date: datetime.date,
     is_holiday: IsHolidayFuncType = global_default_holiday_discriminator,
 ) -> bool:
+    """Check if the given date is the first business day of the month.
+
+    Args:
+        date (datetime.date): Date to check.
+        is_holiday (IsHolidayFuncType, optional): Function to check if a date is a holiday.
+            Defaults to global_default_holiday_discriminator.
+
+    Returns:
+        bool: True if the date is the first business day of the month, False otherwise.
+    """  # noqa: E501
     if is_holiday(date):
         return False
     return date.month != (get_prev_bizday(date, is_holiday)).month
@@ -32,6 +52,16 @@ def get_biz_end_of_month(
     date: datetime.date,
     is_holiday: IsHolidayFuncType = global_default_holiday_discriminator,
 ) -> datetime.date:
+    """Get the last business day of the month for the given date.
+
+    Args:
+        date (datetime.date): reference date.
+        is_holiday (IsHolidayFuncType, optional): function to check if a date is a holiday.
+            Defaults to global_default_holiday_discriminator.
+
+    Returns:
+        datetime.date: last business day of the month for the given date.
+    """  # noqa: E501
     date = date.replace(day=1)  # start of month
     date = date + datetime.timedelta(days=31)
     date = date.replace(day=1)  # start of next month
@@ -42,6 +72,16 @@ def get_biz_start_of_month(
     date: datetime.date,
     is_holiday: IsHolidayFuncType = global_default_holiday_discriminator,
 ) -> datetime.date:
+    """Get the first business day of the month for the given date.
+
+    Args:
+        date (datetime.date): reference date.
+        is_holiday (IsHolidayFuncType, optional): function to check if a date is a holiday.
+            Defaults to global_default_holiday_discriminator.
+
+    Returns:
+        datetime.date: first business day of the month for the given date.
+    """  # noqa: E501
     date = date.replace(day=1)  # start of month
     date = date - datetime.timedelta(days=1)  # end of previous month
     return get_next_bizday(date, is_holiday)
@@ -56,6 +96,22 @@ def add_years_months(
     bizeom2bizeom: bool = True,
     bizsom2bizsom: bool = False,
 ) -> datetime.date:
+    """Add years and months to a date with business day adjustment.
+
+    Args:
+        date (datetime.date): Reference date.
+        years (int): Years to add
+        months (int): Months to add
+        is_holiday (IsHolidayFuncType, optional): function to check if a date is a holiday.
+            Defaults to global_default_holiday_discriminator.
+        bizeom2bizeom (bool, optional): Whether to adjust to the last business day of the month
+            if the date is the last business day of the month. Defaults to True.
+        bizsom2bizsom (bool, optional): Whether to adjust to the first business day of the month
+            if the date is the first business day of the month. Defaults to False.
+
+    Returns:
+        datetime.date: Date after adding years and months with business day adjustment.
+    """  # noqa: E501
     added = date + relativedelta(years=years, months=months)
     if bizeom2bizeom and is_biz_end_of_month(date, is_holiday):
         return get_biz_end_of_month(added, is_holiday)
@@ -73,6 +129,22 @@ def add_years(
     bizeom2bizeom: bool = True,
     bizsom2bizsom: bool = False,
 ) -> datetime.date:
+    """Add years to a date with business day adjustment.
+    This function is a wrapper around add_years_months with months set to 0.
+
+    Args:
+        date (datetime.date): Reference date.
+        years (int): Years to add
+        is_holiday (IsHolidayFuncType, optional): function to check if a date is a holiday.
+            Defaults to global_default_holiday_discriminator.
+        bizeom2bizeom (bool, optional): Whether to adjust to the last business day of the month
+            if the date is the last business day of the month. Defaults to True.
+        bizsom2bizsom (bool, optional): Whether to adjust to the first business day of the month
+            if the date is the first business day of the month. Defaults to False.
+
+    Returns:
+        datetime.date: Date after adding years with business day adjustment.
+    """  # noqa: E501
     return add_years_months(
         date,
         years,
@@ -91,6 +163,22 @@ def add_months(
     bizeom2bizeom: bool = True,
     bizsom2bizsom: bool = False,
 ) -> datetime.date:
+    """Add months to a date with business day adjustment.
+    This function is a wrapper around add_years_months with years set to 0.
+
+    Args:
+        date (datetime.date): Reference date.
+        months (int): Months to add
+        is_holiday (IsHolidayFuncType, optional): function to check if a date is a holiday.
+            Defaults to global_default_holiday_discriminator.
+        bizeom2bizeom (bool, optional): Whether to adjust to the last business day of the month
+            if the date is the last business day of the month. Defaults to True.
+        bizsom2bizsom (bool, optional): Whether to adjust to the first business day of the month
+            if the date is the first business day of the month. Defaults to False.
+
+    Returns:
+        datetime.date: Date after adding months with business day adjustment.
+    """  # noqa: E501
     return add_years_months(
         date,
         0,
